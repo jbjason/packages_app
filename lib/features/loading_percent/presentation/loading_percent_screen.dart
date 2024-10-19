728c7f43c4c671aadc9f3b0e08e2806a7434041e
jubayer040 <132200432+jubayer040@users.noreply.github.com>
2024-10-19 21:05:37 +0600

    loading percent added

---
+++ lib/features/loading_percent/presentation/loading_percent_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:packages_app/core/util/mycolor.dart';
+import 'package:packages_app/core/util/mydimens.dart';
+
+enum LoadingType { horizontal, vertical, circular }
+
+class LoadingPercentScreen extends StatelessWidget {
+  const LoadingPercentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: MyDimens().getNormalAppBar("Loading Percent", [], context),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(12),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                _getHorizontalVerticalLoading(
+                  type: LoadingType.horizontal,
+                  height: 30,
+                  width: size.width,
+                  loadingPercent: .3,
+                ),
+                _getHorizontalVerticalLoading(
+                    type: LoadingType.vertical,
+                    height: 80,
+                    width: 30,
+                    loadingPercent: .6),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getHorizontalVerticalLoading({
+    required LoadingType type,
+    required double height,
+    required double width,
+    Color backColor = MyColor.inActiveColor,
+    double borderRadius = 12,
+    Color indicatorColor = MyColor.bluePrimary,
+    required double loadingPercent,
+  }) {
+    bool isHorizontal = type == LoadingType.horizontal;
+    final indicatorHeightPercent =
+        isHorizontal ? height : _getPercent(height, loadingPercent);
+    final indicatorWidthPercent =
+        isHorizontal ? _getPercent(width, loadingPercent) : width;
+    final indicatorAlignment =
+        isHorizontal ? Alignment.centerLeft : Alignment.bottomCenter;
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius),
+        color: backColor,
+      ),
+      child: Container(
+        height: indicatorHeightPercent,
+        width: indicatorWidthPercent,
+        alignment: indicatorAlignment,
+        // constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(borderRadius),
+          color: indicatorColor,
+        ),
+      ),
+    );
+  }
+
+  double _getPercent(double width, double loadingPercent) {
+    if (loadingPercent > 1) return width;
+    if (loadingPercent < 0) return 0;
+    return width * loadingPercent;
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:packages_app/features/loading_percent/presentation/loading_percent_screen.dart';
 import 'package:packages_app/features/otp_field/presentation/otp_field_screen.dart';
 import 'package:packages_app/features/shimmer_loading/presentation/shimmer_loading_screen.dart';
 
@@ -9,26 +10,41 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => ShimmerLoadingScreen()));
-              },
-              child: Text("Shimmer Loading Package"),
-            ),
-            const SizedBox(height: 15),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => OtpFieldScreen()));
-              },
-              child: Text("OTP Field Package"),
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => ShimmerLoadingScreen()));
+                },
+                child: Text("Shimmer Loading Package"),
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => OtpFieldScreen()));
+                },
+                child: Text("OTP Field Package"),
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => LoadingPercentScreen()));
+                },
+                child: Text("Loading Percent Package"),
+              ),
+            ],
+          ),
         ),
       ),
     );
