3078175c3b0b1f44c14ef0d0a93fc193fa89ad55
jubayer040 <135593620+JubayerAl040@users.noreply.github.com>
2024-11-11 16:53:25 +0600

    device-info & dynamic function done

--- lib/features/device_info/presentation/device_info_screen.dart
+++ lib/features/device_info/presentation/device_info_screen.dart
@@ -2,18 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:packages_app/core/util/mydimens.dart';
 import 'package:packages_app/features/device_info/data/data_sources/device_info_source.dart';
-
-import 'package:packages_app/features/device_info/data/repository/device_info_repo.dart';
 import 'package:packages_app/features/device_info/presentation/providers/device_info_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class DeviceInfoScreen extends StatefulWidget {
+class DeviceInfoScreen extends StatelessWidget {
   const DeviceInfoScreen({super.key, required this.onSubmit});
   final Function(dynamic) onSubmit;
-  @override
-  State<DeviceInfoScreen> createState() => _DeviceInfoScreenState();
-}
-
-class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +37,10 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
             MyDimens.cmDivider,
             const SizedBox(height: 10),
             ElevatedButton(
-              onPressed: () => _getInfo(DeviceInfoTypeEnum.contactList),
+              onPressed: () async {
+                await Permission.contacts.request();
+                _getInfo(DeviceInfoTypeEnum.contactList);
+              },
               child: Text("Get Contact List"),
             ),
           ],
@@ -53,78 +50,7 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
   }
 
   Future<void> _getInfo(DeviceInfoTypeEnum enumType) async {
-    String type = "";
-    switch (enumType) {
-      case DeviceInfoTypeEnum.deviceInfo:
-        {
-          type = DeviceInfoRepo.typeDeviceInfo;
-          break;
-        }
-      case DeviceInfoTypeEnum.battery:
-        {
-          type = DeviceInfoRepo.typeBatteryInfo;
-          break;
-        }
-      case DeviceInfoTypeEnum.networkStatus:
-        {
-          type = DeviceInfoRepo.typeNetworkInfo;
-          break;
-        }
-      case DeviceInfoTypeEnum.contactList:
-        {
-          type = DeviceInfoRepo.typeContactInfo;
-          break;
-        }
-      default:
-        null;
-    }
-    final result = await DeviceInfoProvider.getInfo(enumType, type);
-
+    final result = await DeviceInfoProvider().getInfo(enumType);
     Logger().f(result);
   }
-
-  // Future<void> _getDeviceInfo() async {
-  //   try {
-  //     await DeviceInfoRepo.platform
-  //         .invokeMethod(DeviceInfoRepo.typeDeviceInfo)
-  //         .then((value) {
-  //       _deviceInfo = value;
-  //     });
-  //   } on PlatformException catch (e) {
-  //     _deviceInfo = e.message!;
-  //   }
-  //   setState(() {});
-  // }
-
-  // Future<void> _getBatteryLevel() async {
-  //   try {
-  //     await DeviceInfoRepo.platform
-  //         .invokeMethod<int>(DeviceInfoRepo.typeBatteryInfo)
-  //         .then((val) => _batteryInfo = '$val%');
-  //   } on PlatformException catch (e) {
-  //     _batteryInfo = "Failed to get battery level: '${e.message}'.";
-  //   }
-  //   setState(() {});
-  // }
-
-  // Future<void> _getNetworkInfo() async {
-  //   try {
-  //     await DeviceInfoRepo.platform
-  //         .invokeMethod<String>(DeviceInfoRepo.typeNetworkInfo)
-  //         .then((val) => _networkInfo = val!);
-  //   } on PlatformException catch (e) {
-  //     _networkInfo = e.message!;
-  //   }
-  //   setState(() {});
-  // }
-
-  // Future<void> _getContactInfo() async {
-  //   try {
-  //     await DeviceInfoRepo.platform
-  //         .invokeMethod<String>(DeviceInfoRepo.typeContactInfo)
-  //         .then((val) => developer.log(val!));
-  //   } on PlatformException catch (e) {
-  //     Logger().e(e);
-  //   }
-  // }
 }
--- lib/features/device_info/presentation/providers/device_info_provider.dart
+++ lib/features/device_info/presentation/providers/device_info_provider.dart
@@ -1,87 +1,143 @@
-import 'dart:convert';
-
 import 'package:flutter/services.dart';
 import 'package:logger/logger.dart';
 import 'package:packages_app/features/device_info/data/data_sources/device_info_source.dart';
 import 'package:packages_app/features/device_info/data/models/device_info.dart';
 import 'package:packages_app/features/device_info/data/models/device_info_contact.dart';
 import 'package:packages_app/features/device_info/data/repository/device_info_repo.dart';
-import 'dart:developer' as developer;
 
 class DeviceInfoProvider {
-  static Future<dynamic> getInfo(
-      DeviceInfoTypeEnum enumType, String type) async {
+  Future<dynamic> getInfo(DeviceInfoTypeEnum enumType) async {
     try {
-      await DeviceInfoRepo.platform.invokeMethod(type).then((result) {
-        developer.log(result);
-        dynamic response;
-        switch (enumType) {
-          case DeviceInfoTypeEnum.deviceInfo:
-            {
-              Logger().i(result);
-              final item =
-                  result.toString().substring(1, result.toString().length - 1);
-              print(item);
-              final items = item.split(", ");
-              print(items);
-              Logger().t("""
-                id: ${items[0]},
-                version: ${items[1]},
-                device: ${items[2]},
-                model: ${items[3]},
-                product:${items[4]},
-                manufacturer: ${items[5]},
-                sdkVersion: ${items[6]},
-""");
-              response = DeviceInfo(
-                  id: items[0],
-                  version: items[1],
-                  device: items[2],
-                  model: items[3],
-                  product: items[4],
-                  manufacturer: items[5],
-                  sdkVersion: items[6]);
-              print(response.id);
-              print(response.version);
-              print(response.device);
-              print(response.model);
-              print(response.product);
-              print(response.manufacturer);
-              print(response.sdkVersion);
-              break;
-            }
-          case DeviceInfoTypeEnum.battery:
-            {
-              response = '$result%';
-              break;
-            }
-          case DeviceInfoTypeEnum.networkStatus:
-            {
-              response = result;
-              break;
-            }
-          case DeviceInfoTypeEnum.contactList:
-            {
-              for (int i = 0; i < result.length; i++) {
-                response.add(
+      dynamic response;
+      switch (enumType) {
+        case DeviceInfoTypeEnum.deviceInfo:
+          {
+            final result =
+                await _getInfoFromDevice(DeviceInfoRepo.typeDeviceInfo);
+            final item =
+                result.toString().substring(1, result.toString().length - 1);
+            final items = item.split(", ");
+            response = DeviceInfo(
+              id: items[0],
+              version: items[1],
+              device: items[2],
+              model: items[3],
+              product: items[4],
+              manufacturer: items[5],
+              sdkVersion: items[6],
+            );
+            break;
+          }
+        case DeviceInfoTypeEnum.battery:
+          {
+            response = await _getInfoFromDevice(DeviceInfoRepo.typeBatteryInfo);
+            break;
+          }
+        case DeviceInfoTypeEnum.networkStatus:
+          {
+            response = await _getInfoFromDevice(DeviceInfoRepo.typeNetworkInfo);
+            break;
+          }
+        case DeviceInfoTypeEnum.contactList:
+          {
+            final result =
+                await _getInfoFromDevice(DeviceInfoRepo.typeContactInfo);
+            if (result != null || result.toString().length > 5) {
+              final List<DeviceInfoContact> contactList = [];
+              final item = result.toString().substring(1, result.length - 2);
+              final items = item.split("], ");
+              Logger().e(items);
+              for (int i = 0; i < items.length; i++) {
+                final item2 = items[i]
+                    .toString()
+                    .substring(1, items[i].toString().length);
+                final items2 = item2.split(", ");
+                Logger().e(items2);
+                contactList.add(
                   DeviceInfoContact(
-                    id: (result[i][0]).toString(),
-                    name: (result[i][1]).toString(),
-                    number: (result[i][2]).toString(),
-                    image: result[i][3]!.toString(),
+                    id: (items2[0]),
+                    name: (items2[1]),
+                    number: (items2[2]),
+                    image: items2[3],
                   ),
                 );
               }
-              break;
+              response = contactList;
             }
-          default:
-            print("not Matched");
             break;
-        }
-        return response;
-      });
+          }
+        default:
+          null;
+      }
+      return response;
+      // await DeviceInfoRepo.platform.invokeMethod(type).then((result) {
+      //   developer.log(result);
+      //   switch (enumType) {
+      //     case DeviceInfoTypeEnum.deviceInfo:
+      //       {
+      //         Logger().i(result);
+      //         final item =
+      //             result.toString().substring(1, result.toString().length - 1);
+      //         final items = item.split(", ");
+      //         response = DeviceInfo(
+      //             id: items[0],
+      //             version: items[1],
+      //             device: items[2],
+      //             model: items[3],
+      //             product: items[4],
+      //             manufacturer: items[5],
+      //             sdkVersion: items[6]);
+      //         print(response.id);
+      //         print(response.version);
+      //         print(response.device);
+      //         print(response.model);
+      //         print(response.product);
+      //         print(response.manufacturer);
+      //         print(response.sdkVersion);
+      //         break;
+      //       }
+      //     case DeviceInfoTypeEnum.battery:
+      //       {
+      //         response = '$result%';
+      //         break;
+      //       }
+      //     case DeviceInfoTypeEnum.networkStatus:
+      //       {
+      //         response = result;
+      //         break;
+      //       }
+      //     case DeviceInfoTypeEnum.contactList:
+      //       {
+      //         for (int i = 0; i < result.length; i++) {
+      //           response.add(
+      //             DeviceInfoContact(
+      //               id: (result[i][0]).toString(),
+      //               name: (result[i][1]).toString(),
+      //               number: (result[i][2]).toString(),
+      //               image: result[i][3]!.toString(),
+      //             ),
+      //           );
+      //         }
+      //         break;
+      //       }
+      //     default:
+      //       print("not Matched");
+      //       break;
+      //   }
+      // });
+      // return response;
     } on PlatformException catch (e) {
       return e.message!;
+    }
+  }
+
+  static dynamic _getInfoFromDevice(String type) async {
+    try {
+      final result = await DeviceInfoRepo.platform.invokeMethod(type);
+      Logger().t(result);
+      return result;
+    } catch (e) {
+      Logger().e(e);
     }
   }
 }
